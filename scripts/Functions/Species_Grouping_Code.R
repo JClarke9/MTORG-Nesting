963dc02c469d4f53088bee7266b3bb0ddeaf5028
JClarke9 <31852057+JClarke9@users.noreply.github.com>
2025-04-11 10:18:49 -0500

    re-writing this so it works by specifying the column name.

--- scripts/Functions/Species_Grouping_Code.R
+++ scripts/Functions/Species_Grouping_Code.R
@@ -1,60 +1,60 @@
 # Species Grouping Function
 
 # create a column for each data frame with the associated group information
-SpeciesGrouping <- function(x) {
-  x <- x |>                                                                     
-    mutate(Group = case_when(                                                   
+SpeciesGrouping <- function(x, colname) {
+  x <- x |>
+    mutate(Group = case_when(
       # Grebes
-      endsWith(Spec, "PBGR") ~ "WET",  # Pied-billed Grebe
+      endsWith(as.character(x[[colname]]), "PBGR") ~ "WET",  # Pied-billed Grebe
       
       # Herons & Bitterns
-      endsWith(Spec, "AMBI") ~ "FAC",  # American Bittern
+      endsWith(as.character(x[[colname]]), "AMBI") ~ "FAC",  # American Bittern
       
       # Waterfowl
-      endsWith(Spec, "MALL") ~ "FAC",  # Mallard
-      endsWith(Spec, "GADW") ~ "FAC",  # Gadwall
-      endsWith(Spec, "NOPI") ~ "FAC",  # Northern Pintail
-      endsWith(Spec, "AMWI") ~ "FAC",  # American Wigeon
-      endsWith(Spec, "NSHO") ~ "FAC",  # Northern Shoveler
-      endsWith(Spec, "BWTE") ~ "FAC",  # Blue-winged Teal
-      endsWith(Spec, "GWTE") ~ "FAC",  # Green-winged Teal
-      endsWith(Spec, "RNDU") ~ "WET",  # Ring-necked Duck
-      endsWith(Spec, "LESC") ~ "FAC",  # Lesser Scaup
+      endsWith(as.character(x[[colname]]), "MALL") ~ "FAC",  # Mallard
+      endsWith(as.character(x[[colname]]), "GADW") ~ "FAC",  # Gadwall
+      endsWith(as.character(x[[colname]]), "NOPI") ~ "FAC",  # Northern Pintail
+      endsWith(as.character(x[[colname]]), "AMWI") ~ "FAC",  # American Wigeon
+      endsWith(as.character(x[[colname]]), "NSHO") ~ "FAC",  # Northern Shoveler
+      endsWith(as.character(x[[colname]]), "BWTE") ~ "FAC",  # Blue-winged Teal
+      endsWith(as.character(x[[colname]]), "GWTE") ~ "FAC",  # Green-winged Teal
+      endsWith(as.character(x[[colname]]), "RNDU") ~ "WET",  # Ring-necked Duck
+      endsWith(as.character(x[[colname]]), "LESC") ~ "FAC",  # Lesser Scaup
       
       # Grouse
-      endsWith(Spec, "STGR") ~ "OBL",  # Sharp-tailed Grouse
+      endsWith(as.character(x[[colname]]), "STGR") ~ "OBL",  # Sharp-tailed Grouse
       
       # Rails & Coots
-      endsWith(Spec, "AMCO") ~ "WET",  # American Coot
-      endsWith(Spec, "SORA") ~ "WET",  # Sora
+      endsWith(as.character(x[[colname]]), "AMCO") ~ "WET",  # American Coot
+      endsWith(as.character(x[[colname]]), "SORA") ~ "WET",  # Sora
       
       # Shorebirds
-      endsWith(Spec, "KILL") ~ "FAC",  # Killdeer
-      endsWith(Spec, "WILL") ~ "FAC",  # Willet
-      endsWith(Spec, "UPSA") ~ "OBL",  # Upland Sandpiper
-      endsWith(Spec, "WISN") ~ "WET",  # Wilson's Snipe
-      endsWith(Spec, "WIPH") ~ "WET",  # Wilson's Phalarope
+      endsWith(as.character(x[[colname]]), "KILL") ~ "FAC",  # Killdeer
+      endsWith(as.character(x[[colname]]), "WILL") ~ "FAC",  # Willet
+      endsWith(as.character(x[[colname]]), "UPSA") ~ "OBL",  # Upland Sandpiper
+      endsWith(as.character(x[[colname]]), "WISN") ~ "WET",  # Wilson's Snipe
+      endsWith(as.character(x[[colname]]), "WIPH") ~ "WET",  # Wilson's Phalarope
       
       # Doves
-      endsWith(Spec, "MODO") ~ "FAC",  # Mourning Dove
+      endsWith(as.character(x[[colname]]), "MODO") ~ "FAC",  # Mourning Dove
       
       # Kingbirds & Wrens
-      endsWith(Spec, "EAKI") ~ "FAC",  # Eastern Kingbird
-      endsWith(Spec, "MAWR") ~ "WET",  # Marsh Wren
+      endsWith(as.character(x[[colname]]), "EAKI") ~ "FAC",  # Eastern Kingbird
+      endsWith(as.character(x[[colname]]), "MAWR") ~ "WET",  # Marsh Wren
       
       # Sparrows & Longspurs
-      endsWith(Spec, "DICK") ~ "OBL",  # Dickcissel
-      endsWith(Spec, "CCSP") ~ "FAC",  # Clay-colored Sparrow
-      endsWith(Spec, "GRSP") ~ "OBL",  # Grasshopper Sparrow
-      endsWith(Spec, "SAVS") ~ "OBL",  # Savannah Sparrow
-      endsWith(Spec, "CCLO") ~ "OBL",  # Chestnut-collared Longspur
+      endsWith(as.character(x[[colname]]), "DICK") ~ "OBL",  # Dickcissel
+      endsWith(as.character(x[[colname]]), "CCSP") ~ "FAC",  # Clay-colored Sparrow
+      endsWith(as.character(x[[colname]]), "GRSP") ~ "OBL",  # Grasshopper Sparrow
+      endsWith(as.character(x[[colname]]), "SAVS") ~ "OBL",  # Savannah Sparrow
+      endsWith(as.character(x[[colname]]), "CCLO") ~ "OBL",  # Chestnut-collared Longspur
       
       # Blackbirds
-      endsWith(Spec, "WEME") ~ "OBL",  # Western Meadowlark
-      endsWith(Spec, "BOBO") ~ "OBL",  # Bobolink
-      endsWith(Spec, "YHBL") ~ "WET",  # Yellow-headed Blackbird
-      endsWith(Spec, "RWBL") ~ "FAC",  # Red-winged Blackbird
-      endsWith(Spec, "BRBL") ~ "FAC",  # Brewer's Blackbird
-      endsWith(Spec, "COGR") ~ "FAC"   # Common Grackle
+      endsWith(as.character(x[[colname]]), "WEME") ~ "OBL",  # Western Meadowlark
+      endsWith(as.character(x[[colname]]), "BOBO") ~ "OBL",  # Bobolink
+      endsWith(as.character(x[[colname]]), "YHBL") ~ "WET",  # Yellow-headed Blackbird
+      endsWith(as.character(x[[colname]]), "RWBL") ~ "FAC",  # Red-winged Blackbird
+      endsWith(as.character(x[[colname]]), "BRBL") ~ "FAC",  # Brewer's Blackbird
+      endsWith(as.character(x[[colname]]), "COGR") ~ "FAC"   # Common Grackle
     ))
 }
