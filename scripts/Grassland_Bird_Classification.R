ce1e7eb4bd06994ac6c9981b92ebc7e7c70958c5
Justin Clarke <justin.meadowclarke@gmail.com>
2026-04-30 11:37:29 -0500

    Adding some bird classification

--- scripts/Grassland_Bird_Classification.R
+++ scripts/Grassland_Bird_Classification.R
@@ -6,17 +6,20 @@
 library(readxl)
 library(tidyverse)
 
-raw <- read_xlsx("raw/ACAD Global 2024.05.23.xlsx")
-
+raw <- read_xlsx("raw/ACAD Global 2024.05.23.xlsx") |> 
+  dplyr::select(`Common Name`, `Scientific Name`, USA, Canada, Mexico, `C America`, `Primary Breeding Habitat`, `Secondary Breeding Habitat`) 
+  
 
 # Data Wrangling ----------------------------------------------------------
 
 
 grassland <- raw |> 
-  filter(str_detect(`Primary Breeding Habitat`, "Grasslands:") & str_detect(`Secondary Breeding Habitat`, "Grasslands:"))
-
-grassland <- raw |> 
-  dplyr::select(`Common Name`, `Scientific Name`, USA, Canada, Mexico, `C America`, `Primary Breeding Habitat`, `Secondary Breeding Habitat`) |> 
   filter(`Primary Breeding Habitat` %in% c("Grasslands:  Chihuahuan", "Grasslands:  Temperate", "Grasslands:  Tropical" ) |
            `Secondary Breeding Habitat` %in% c("Grasslands:  Chihuahuan", "Grasslands:  Temperate", "Grasslands:  Tropical", "Grasslands:  Pampas and Campos") &
            (USA == 1 | Canada == 1 | `C America` == 1))
+
+mtorg <- raw |> 
+  filter(`Common Name` %in% c("Brewer's Blackbird", "Red-winged Blackbird", "Yellow-headed Blackbird", 
+                              "Western Meadowlark", "Mourning Dove", "Clay-colored Sparrow",
+                              "Gadwall", "Northern Pintail", "Blue-winged Teal", "Northern Shoveler",
+                              "Mallard"))
